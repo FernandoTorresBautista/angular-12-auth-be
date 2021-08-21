972e036cbe627fa3b6feb7fd46e890d736421234
Fernando Torres <fernandot371@gmail.com>
2021-08-21 16:42:50 -0500

    quit public/ & modify index.js manage others pathsURLs

--- index.js
+++ index.js
@@ -30,6 +30,11 @@ app.get('/ping', (req, res)=>{
 // middleware router
 app.use('/api/auth/', require('./routes/auth'));
 
+// Manejar demas rutas
+app.get('*', (req, res) => {
+  res.sendFile( path.resolve(__dirname, 'public/index.html') );
+});
+
 // 
 app.listen( process.env.PORT, () => {
   console.log(`Servidor corriendo en puerto ${ process.env.PORT }`);
